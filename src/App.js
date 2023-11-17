d4ca7c508caff0eb777c0c34ad6a6e350108d058
KaroGil <karolinkagil@gmail.com>
2023-11-17 15:20:53 +0100

    remove dobble heading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css';
 import { DaysUntil } from './components/DaysUntil';
 
 function App() {
-  const movie = "The hunger games: The ballad of songbirds and snakes"
   return (
     <div className="App">
-      <h1>Days Until</h1>
-      <h2>{movie}</h2>
        <DaysUntil date="2023-11-16" />
     </div>
   );
