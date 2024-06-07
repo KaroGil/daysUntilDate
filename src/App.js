793ec836fdb532a40dce6add9e522afa12363d26
KaroGil <karolinkagil@gmail.com>
2024-06-07 12:45:26 +0200

    new book added, toggle function added, calender added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 import { DaysUntil } from './components/DaysUntil';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => {
+    setToggle(!toggle);
+  }
+
+
+  const events = [
+    { date: "2023-11-16", movie: "The hunger games: The ballad of songbirds and snakes" },
+    { date: "2025-03-18", movie: "The hunger games: Sunrise on the Reaping (Book)" },
+    { date: "2026-11-20", movie: "The hunger games: Sunrise on the Reaping (Movie)" }
+  ];
+
+  const today = new Date();
+  const filteredEvents = events.filter(event => {
+    const eventDate = new Date(event.date);
+    return toggle ? eventDate >= today : true;
+  });
+
+
   return (
     <div className="App">
-       <DaysUntil date="2023-11-16" />
+      <button className="toggle" onClick={handleClick}>
+        {toggle ? 'Show All Events' : 'Show Upcoming Events Only'}
+      </button>
+      {filteredEvents.map(event => (
+        <DaysUntil key={event.date} date={event.date} movie={event.movie} />
+      ))}
     </div>
   );
 }
--- src/components/DaysUntil.js
+++ src/components/DaysUntil.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { AddToCalendarButton } from 'add-to-calendar-button-react';
 
-export const DaysUntil = (date) => {
+export const DaysUntil = ({date, movie}) => {
 
-   const movie = "The hunger games: The ballad of songbirds and snakes"
+   //const movie = "The hunger games: The ballad of songbirds and snakes"
 
     // calculation of days remaining
-    const targetDate = new Date(date.date);
+    console.log(date)
+    const targetDate = new Date(date);
     const currentDate = new Date();
     const timeDifference = targetDate - currentDate;
     const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
@@ -15,6 +17,17 @@ export const DaysUntil = (date) => {
       {daysRemaining < 0 ? <h1>Days Since</h1> : <h1>Days Until</h1>}
       <h2>{movie}</h2>
       { daysRemaining === 0 ? <h1 id='remaining'>Today</h1> : <h1 id='remaining'>{Math.abs(daysRemaining)}</h1>}
+      { Math.abs(daysRemaining) === 1 ? <h2 className='day'>day</h2> : <h2 className='day'>days</h2>}
+
+      <AddToCalendarButton
+        name={movie}
+        options={'Google'}
+        location="World Wide Web"
+        startDate={date}
+        endDate={date}
+        hideCheckmark={true}
+        lightMode='body'
+      ></AddToCalendarButton>
     </div>
   );
 }
